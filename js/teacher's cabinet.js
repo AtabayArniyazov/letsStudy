1c34e4e0d50f4f5fb2887cd0bd0b2feef291f2e4
Atabay Arniyazov <atabayarniyazov@gmail.com>
2017-07-23 06:52:43 +0300

    teacher's cabinet

--- js/teacher's cabinet.js
+++ js/teacher's cabinet.js
@@ -1,5 +1,21 @@
 "use strict"
 
+// ------------------------------------------------------------------------------
+var header = document.getElementById("header");
+
+header.addEventListener("click", wichContent, false);
+
+function wichContent(EO) {
+	EO = EO || window.event;
+	console.log("Id кликнутого элемента " + EO.target.id);
+
+	var targetID = EO.target.id;
+
+	document.getElementById('content').innerHTML = "";
+	document.getElementById('content').innerHTML = document.getElementById(targetID + "Content").innerHTML;
+}
+// ------------------------------------------------------------------------------
+
 window.onload = function(){
     (function showTime(){
         var date = new Date();
@@ -13,13 +29,3 @@ window.onload = function(){
 $(document).ready(function() {
     $(".dropdown-toggle").dropdown();
 });
-
-function notice1() {
-	document.getElementById('content').innerHTML = "";
-	document.getElementById('content').innerHTML = document.getElementById('notice1').innerHTML;
-}
-
-function message1() {
-	document.getElementById('content').innerHTML = "";
-	document.getElementById('content').innerHTML = document.getElementById('message1').innerHTML;
-}
