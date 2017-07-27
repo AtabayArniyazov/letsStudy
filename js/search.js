38d4b30d162d8d632ccab236e90dd4735701fcc8
Atabay Arniyazov <atabayarniyazov@gmail.com>
2017-07-27 12:13:34 +0300

    min changed; add video for teacher, sideDiv fixed right & top when scrolling; done 'teachers_page_prototype.psd;

--- js/search.js
+++ js/search.js
@@ -13,3 +13,47 @@ $(document).ready(function() {
 	// рейтинг звезд
 	
 });
+
+function teachersPage(EO) {
+	EO = EO || window.event;
+	console.log("Id кликнутого элемента " + EO.target.id);
+	
+	function determineTimeZone() {
+		var date = new Date(),
+			timeZone;
+
+		if (date.getHours() > date.getUTCHours()) {
+			timeZone = " (GMT+" + (Number(date.getHours()) - Number(date.getUTCHours())) + ")";
+		}
+		if (date.getHours() < date.getUTCHours()) {
+			timeZone = " (GMT" + (Number(date.getHours()) - Number(date.getUTCHours())) + ")";
+		}
+		if (date.getHours() == date.getUTCHours()) {
+			timeZone = " (GMT 0";
+		}
+
+		return timeZone;
+	}
+
+	document.getElementById("mainContent").innerHTML = document.getElementById("teachersPageContent").innerHTML;
+	document.getElementById("timeAndTimezone").innerHTML = determineTimeZone();
+
+
+	// sideDiv position fixed when scrolling and sideDiv => top=0
+	var elemPos = document.getElementById("aside").getBoundingClientRect();
+
+	window.onscroll = function() {
+		var scrollPos = document.getElementById("aside").getBoundingClientRect();
+
+		if (scrollPos.top < 10) {
+			document.getElementById("aside").style.position = "fixed";
+			document.getElementById("aside").style.top = 0;
+			document.getElementById("aside").style.width = "360px";
+		}
+
+		if (document.getElementById("wrap").getBoundingClientRect().top > 0) {
+			document.getElementById("aside").style.position = "static";
+			document.getElementById("aside").style.top = document.getElementById("wrap").getBoundingClientRect().top;
+		}
+	}
+}
